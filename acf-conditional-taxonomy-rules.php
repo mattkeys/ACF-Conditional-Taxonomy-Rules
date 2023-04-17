d3443c42cbd8402877e4638c61fbbd9073b45a2b
MadMaxInfinity <77725769+MadMaxInfinity@users.noreply.github.com>
2023-04-17 07:38:27 +0200

    Added needed variables for translation

--- acf-conditional-taxonomy-rules.php
+++ acf-conditional-taxonomy-rules.php
@@ -3,9 +3,11 @@
 Plugin Name: ACF Conditional Taxonomy Rules
 GitHub Plugin URI: https://github.com/mattkeys/ACF-Conditional-Taxonomy-Rules
 Description: Allow conditional rules to use 'taxonomy' field types
-Version: 3.0.1
-Author: mattkeys
+Version: 3.0.1.1
+Author: mattkeys, MadMaxInfinity
 Author URI: http://mattkeys.me
+Text Domain: acf-conditional-taxonomy-rules
+Domain Path: /languages/
 License: GPL2
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 */
@@ -23,6 +25,11 @@ if ( ! defined( 'ACFCTR_DIRECTORY' ) ) {
 // Publicly Accessible path
 if ( ! defined( 'ACFCTR_PUBLIC_PATH' ) ) {
 	define( 'ACFCTR_PUBLIC_PATH', plugin_dir_url( __FILE__ ) );
+}
+
+// Name to the plugin's directory
+if ( ! defined( 'ACFCTR_DIRECTORY_NAME' ) ) {
+	define( 'ACFCTR_DIRECTORY_NAME', dirname(plugin_basename(__FILE__)));
 }
 
 // Load the plugin
